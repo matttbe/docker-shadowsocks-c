a84fbf0d85f2cc12dce2a2ca6551a6a4f4e2bdc7
Matthieu Baerts <matttbe@gmail.com>
2014-12-27 16:29:18 +0100

    launch: can add extras args

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 [ "$1" = "" ] && PORT=8388 || PORT=$1
-docker run -d -p $PORT:8388 -P --name shadowsocks-c matttbe/shadowsocks-c -c /etc/shadowsocks-libev/config.json
+docker run -d -p $PORT:8388 -P --name shadowsocks-c matttbe/shadowsocks-c -c /etc/shadowsocks-libev/config.json $2
